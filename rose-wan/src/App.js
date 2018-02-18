0ea31c3f1a86ec40eb04e88a57a364a18d446787
rosexw <rose.wan@evermight.com>
2018-02-18 13:31:26 -0500

    input form is created (React tutorial) and name is being sent out as an alert, now also showing in console.log

--- rose-wan/src/App.js
+++ rose-wan/src/App.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './Components/Header';
+import NameForm from './Components/NameForm';
+
+// use this to fetch data
+const fetchPokemon = idOrName =>
+  fetch(`http://pokeapi.co/api/v2/pokemon/${idOrName}`)
+    .then(response => response.json())
+    .then(pokemonData => ({
+      name: pokemonData.name,
+      picture: pokemonData.sprites.front_default
+    }));
 
 class App extends Component {
+  handleFormSubmit(name) {
+    console.log("submitted: ", name);
+  }
+
   render() {
     return (
       <div className="App">
-        <Header text="Gotta Fetch em all!" />
+        <Header text="Gotta Fetch 'em all!" />
+        <NameForm handleFormSubmit={this.handleFormSubmit} />
       </div>
     );
   }
