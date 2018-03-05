dff684be71b0f61b4e69ed3aa9988ed450c4e8ea
rosexw <rose.wan@evermight.com>
2018-03-04 21:29:33 -0500

    removed alt tag from an image tag as recommended by the compiler warning.

--- rose-wan/src/Components/Pokemon.js
+++ rose-wan/src/Components/Pokemon.js
@@ -4,7 +4,7 @@ const Pokemon = (props) => {
   return (
     <div className="PokemonDetails">
       <h3 className="display-name">Pokemon: {props.name} </h3>
-      <img className="display-image" src={props.picture} alt="Pokemon picture"></img>
+      <img className="display-image" src={props.picture}></img>
     </div>
   );
 }
