fde4cf174345eaa83b22c97a56b252b708623252
rosexw <rose.wan@evermight.com>
2018-03-04 21:20:07 -0500

    error codes now working

--- rose-wan/src/App.js
+++ rose-wan/src/App.js
@@ -9,20 +9,32 @@ import Loading from './Components/Loading';
 // use this to fetch data
 const fetchPokemon = idOrName =>
   fetch(`https://pokeapi.co/api/v2/pokemon/${idOrName}`)
+    .then(checkStatus)
     .then(response => response.json())
     .then(pokemonData => ({
       name: pokemonData.name,
       picture: pokemonData.sprites && pokemonData.sprites.front_default
-    }));
+}));
     // .catch((error) => {console.error(`No Pokemon by ${pokemonData.name} found`)})
     // ;
 
 const fetchPokemonList = () =>
   fetch(`https://pokeapi.co/api/v2/pokemon/`)
+    .then(checkStatus)
     .then(response => response.json())
     .then(pokemonData => ({
       results: pokemonData.results,
-    }))
+}))
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
 
 class App extends Component {
   constructor(props) {
@@ -54,6 +66,10 @@ class App extends Component {
           results: pokemonData.results,
           loading: false
         }))
+      .catch(error => this.setState({
+        loading: false,
+        error: "Pokemon cannot be fetched. Please type in another Pokemon.",
+      }))
       return;
     };
     this.setState({
@@ -66,8 +82,13 @@ class App extends Component {
       name: pokemonData.name,
       picture: pokemonData.picture,
       loading: false,
-      error: null
-    }));
+      error: null,
+    }))
+    .catch(error => this.setState({
+      //check error status code here to see what kind of errors are produced - like: Pokemon doesn't exist, no Internet, etc.
+      loading: false,
+      error: "Pokemon cannot be fetched. Please type in another Pokemon.",
+    }))
   }
 
   // saveSearchTerm = event => {
